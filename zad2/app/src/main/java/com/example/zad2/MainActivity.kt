9b710f638958845d5e3f136baedd271e006a8a1f
Maciej <maciejkowalik8009@gmail.com>
2024-06-20 21:31:43 +0200

    Poprawione na ViewBinding

--- zad2/app/src/main/java/com/example/zad2/MainActivity.kt
+++ zad2/app/src/main/java/com/example/zad2/MainActivity.kt
@@ -9,24 +9,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.zad2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
     fun sortB(view: View) {
-        val input: EditText = findViewById(R.id.input)
-        val info: TextView = findViewById(R.id.infoText)
-        val before: TextView = findViewById(R.id.beforeText)
-        val after: TextView = findViewById(R.id.afterText)
+        val input: EditText = binding.input
+        val info: TextView = binding.infoText
+        val before: TextView = binding.afterText
+        val after: TextView = binding.afterText
         if (input.text.toString().isNotEmpty()) {
             before.text = input.text.toString()
             val numbers2 = input.text.split(" ")
@@ -56,10 +53,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
     fun wyb(view: View) {
-        val input: EditText = findViewById(R.id.input)
-        val info: TextView = findViewById(R.id.infoText)
-        val before: TextView = findViewById(R.id.beforeText)
-        val after: TextView = findViewById(R.id.afterText)
+        val input: EditText = binding.input
+        val info: TextView = binding.infoText
+        val before: TextView = binding.afterText
+        val after: TextView = binding.afterText
         if (input.text.toString().isNotEmpty()) {
             before.text = input.text.toString()
             val numbers2 = input.text.split(" ")
